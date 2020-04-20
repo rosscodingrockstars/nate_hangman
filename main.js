6aa60a3f2bea30eb4da9cdf723e2a9a869a1b1c5
jenross <ross.m.jennifer@gmail.com>
2020-04-20 11:54:18 -0400

    fixed sound file and js bugs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const winDisplay = document.getElementById("wins");
+const winsDisplay = document.getElementById("wins");
 const lossesDisplay = document.getElementById("losses");
 const guessesLeftDisplay = document.getElementById("guesses-left");
 let wins = 0;
@@ -25,7 +25,7 @@ let selectedWord = "";
 const displayWord = document.getElementById("chosen-food");
 let underscores = [];
 const playButton = document.getElementById("play-again");
-const winSound = newAudio("sounds/win sound 1.zip");
+const winSound = newAudio("sounds/win sound 1.wav");
 //const loseSound = newAudio(" ");
 
 function startGame() {
@@ -47,58 +47,74 @@ function startGame() {
     underscores [i] = "_"
     }
   }
-  console.log(selectedWord);
   displayWord.innerHTML = underscores.join(" ");
   playButton.style.display = "none";
 }
 
-startGame();
-
 function didYouWin() {
-  if (letterCounter === selectedWord.length && guessesLeft >= 0) {
-    wins++;
+
+  if (underscores.join("") === selectedWord && guessesLeft >= 0) {
     winSound.play();
-    winDisplay.innerHTML = wins;
+    wins++;
+  
+    winsDisplay.innerHTML = wins;
     playButton.style.display = "block";
-  } else {
-    if (letterCounter !== selectedWord.length && guessesLeft <= 1) {
-      losses++;
-      // loseSound.play();
-      lossesDisplay.innerHTML = losses;
+  } else if(guessesLeft === 0) {
+      //loseSound.play();
+      losses++; 
+      lossesDisplay.innerHTML = losses; 
       playButton.style.display = "block";
     }
+  
+}
+
+startGame();
+
+function checkGuess(letter) {
+
+  if (event.keyCode >= 65 && event.keyCode <= 90) { 
+
+       let correctLetter = false;
+
+        for (let i = 0; i < selectedWord.length; i++) {
+          if(selectedWord[i] === letter) {
+            correctLetter = true;
+          }
+        }
+
+        if(correctLetter) {
+          for (let i = 0; i < selectedWord.length; i++) {
+            if(selectedWord[i] === letter) {
+              underscores[i] = letter
+              displayWord.innerHTML = underscores.join(" ");
+            }
+          }
+        }
+        else if (lettersGuessed.includes(letter)) {
+          alert("You already guessed that letter.")
+        }
+
+        else {
+          lettersGuessed.push(letter);
+          lettersGuessedDisplay.innerHTML = lettersGuessed.join(", ");
+        }
+
+    
+  } else { 
+    alert("Please guess a letter.");
   }
 }
 
-document.onkeyup = function (event) {
+
+document.onkeyup = function(event) {
   let userGuess = event.key.toLowerCase();
-  if (event.keyCode < 65 || event.keyCode > 90) {
-    alert("Please guess a letter.");
-  }
+
+  checkGuess(userGuess);
   
-  if (selectedWord.indexOf(userGuess) > -1  && event.keyCode >= 65 && event.keyCode <= 90) {
-    for (let i = 0; i < selectedWord.length; i++) {
-      if (selectedWord[i] === userGuess) {
-        underscores[i] = userGuess;
-        displayWord.innerHTML = underscores.join(" ");
-        letterCounter++;
-        didYouWin();
-      }
-    }
-  } else {
-    if (!selectedWord.indexOf(userGuess) > -1 && event.keyCode >= 65 && event.keyCode <= 90 && lettersGuessed.indexOf (userGuess) === -1) {
-      lettersGuessed.push(userGuess);
-      lettersGuessedDisplay.innerHTML = lettersGuessed.join(", ");
-      didYouWin();
-    }
-    else {
-      if (lettersGuessed.indexOf(userGuess) > -1)
-         alert("You already guessed that letter.");
-    }
-  }
-  if (guessesLeft > 0 > -1 && event.keyCode >= 65 && event.keyCode <= 90 && lettersGuessed.indexOf (userGuess) === -1) {
-  
+  if (event.keyCode >= 65 && event.keyCode <= 90 && guessesLeft >= 1) { 
     guessesLeft--;
-    guessesLeftDisplay.innerHTML = guessesLeft;
-   };
- }
+    guessesLeftDisplay.innerHTML = guessesLeft; 
+  }
+
+  didYouWin();
+}
